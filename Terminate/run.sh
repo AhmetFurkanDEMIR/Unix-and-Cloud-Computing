22437ff65682f2bca0a55108371809862a4a155c
Ahmet Furkan DEMIR <54184905+AhmetFurkanDEMIR@users.noreply.github.com>
2021-05-23 19:33:14 +0300

    Delete Terminate directory

--- Terminate/run.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install python3-pip
-python -m pip install --upgrade pip
-pip3 install PyQt5
-
-cp ass.py ~
-
-echo "alias terminate='python3 ~/ass.py'" >> ~/.bashrc
-echo "alias terminate='python3 ~/ass.py'" >> ~/.profile
